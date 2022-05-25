c0897036f9b30b42a9a1dcb9c38e8b35f70805ac
dyush <dyushes@inbox.ru>
2022-05-25 14:33:02 +0500

    Добавил регистрацию

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
 from . import views
+from django.urls import path
 
 urlpatterns = [
     path('', views.index),
-    path('registration', views.registration),
+    path('registr/', views.registr),
     path('login', views.login),
     path('jobvacancy', views.jobvacancy)
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
 from .models import JobVacancy
+from django.shortcuts import render
+from .forms import RegisterUserForm
+from django.contrib.auth.forms import UserCreationForm
 
 
 def index(request):
@@ -7,7 +9,7 @@ def index(request):
 
 
 def registration(request):
-    return render(request, '../static/../templates/main/registration.php')
+    return render(request, '../static/../templates/main/registr.html')
 
 
 def login(request):
@@ -17,3 +19,31 @@ def login(request):
 def jobvacancy(request):
     job_vacancy = JobVacancy.objects.all()
     return render(request, '../static/../templates/main/jobvacancy.html', {'job_vacancy': job_vacancy})
+
+
+# Функция регистрации
+def registr(request):
+    # Массив для передачи данных шаблонны
+    data = {}
+    # Проверка что есть запрос POST
+    if request.method == 'POST':
+        # Создаём форму
+        form = RegisterUserForm(request.POST)
+        # Валидация данных из формы
+        if form.is_valid():
+            # Сохраняем пользователя
+            form.save()
+            # Передача формы к рендару
+            data['form'] = form
+            # Передача надписи, если прошло всё успешно
+            data['res'] = "Всё прошло успешно"
+            # Рендаринг страницы
+            return render(request, 'main/login.html', data)
+    else: # Иначе
+        # Создаём форму
+        form = RegisterUserForm()
+        # Передаём форму для рендеринга
+        data['form'] = form
+        # Рендаринг страницы
+        return render(request, 'main/registr.html', data)
+
